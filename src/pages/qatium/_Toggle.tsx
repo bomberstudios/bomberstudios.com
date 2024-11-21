88cf8217e782ec5b4e6dc6caf5cf805bba8581e9
Ale M <bomberstudios@gmail.com>
2024-11-21 16:05:24 +0100

    Qatium › Toggle component

---
+++ src/pages/qatium/_Toggle.tsx
@@ -0,0 +1,25 @@
+import { AnimatePresence, motion } from 'motion/react';
+import { useState } from 'react';
+import style from './_Toggle.module.css';
+
+export const Toggle = ({ enabled = true }) => {
+  const [isOn, setIsOn] = useState(enabled);
+
+  return (
+    <motion.button
+      onClick={() => setIsOn(!isOn)}
+      className={style.toggle + ' ' + (isOn ? style.toggleOn : style.toggleOff)}
+      animate={{
+        backgroundColor: isOn ? '#62c357' : '#cccccc',
+      }}
+      transition={{
+        duration: 0.5,
+      }}
+    >
+      <motion.div
+        className={style.handle}
+        animate={{ y: isOn ? 0 : 15 }}
+      />
+    </motion.button>
+  )
+}
