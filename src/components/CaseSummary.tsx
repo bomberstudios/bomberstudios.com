594078e48bae11c3d365d273a459c5e97c1b8c08
Ale M <bomberstudios@gmail.com>
2025-02-09 17:36:55 +0100

    Remove blank target

--- src/components/CaseSummary.tsx
+++ src/components/CaseSummary.tsx
@@ -9,7 +9,7 @@ interface CaseSummaryProps {
 export const CaseSummary = ({ title, link = '', client, tools, tldr }: CaseSummaryProps) => {
   return (
     <section className="text case-summary">
-      {link && <h2><a href={link} target="_blank" rel="noreferrer">{title}</a></h2>}
+      {link && <h2><a href={link}>{title}</a></h2>}
       {!link && <h1>{title}</h1>}
       <p className="lead">{tldr}</p>
       <ul>
